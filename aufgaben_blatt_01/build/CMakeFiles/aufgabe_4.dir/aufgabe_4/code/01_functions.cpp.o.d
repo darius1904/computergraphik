6beddee30bf332ba927543a906b788e7a741bde4
Darius Dolha <166997774+darius1904@users.noreply.github.com>
2025-11-22 22:27:51 +0100

    Delete aufgaben_blatt_01/build/CMakeFiles directory

--- aufgaben_blatt_01/build/CMakeFiles/aufgabe_4.dir/aufgabe_4/code/01_functions.cpp.o.d
+++
@@ -1,4 +0,0 @@
-CMakeFiles/aufgabe_4.dir/aufgabe_4/code/01_functions.cpp.o: \
-  /Users/darius/stuttgart/Computergraphik/aufgaben_blatt_01/aufgabe_4/code/01_functions.cpp \
-  /Users/darius/stuttgart/Computergraphik/aufgaben_blatt_01/aufgabe_4/code/source1.hpp \
-  /Users/darius/stuttgart/Computergraphik/aufgaben_blatt_01/aufgabe_4/code/source2.hpp
--- aufgaben_blatt_01/build/CMakeFiles/aufgabe_4.dir/aufgabe_4/code/source1.cpp.o.d
+++
@@ -1,3 +0,0 @@
-CMakeFiles/aufgabe_4.dir/aufgabe_4/code/source1.cpp.o: \
-  /Users/darius/stuttgart/Computergraphik/aufgaben_blatt_01/aufgabe_4/code/source1.cpp \
-  /Users/darius/stuttgart/Computergraphik/aufgaben_blatt_01/aufgabe_4/code/Definition.hpp
